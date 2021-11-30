903686f670a8d25f0e879e3940dae37deea81b36
Aleks Timonova <alekstim01@gmail.com>
2021-11-30 20:49:56 +0300

    Write To File

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 public class main {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
@@ -32,7 +33,23 @@ public class main {
         System.out.println("----------------------------------------");
         System.out.println("time to delete from arrayList  | " + Time.timeDeleteFromCollection(arrayList, ITERATIONS) + " ns");
         System.out.println("----------------------------------------");
+        try(FileWriter writer = new FileWriter("data.txt", true))
+        {
+            writer.write("Method   |   Iterations   |   Time   ");
+            writer.append('\n');
+            writer.write("Add      |" + ITERATIONS +"           |"+  Time.timeAddToCollection(linkedList, ITERATIONS)+'\n');
+            writer.write("Add      |" + ITERATIONS +"           |"+  Time.timeAddToCollection(arrayList, ITERATIONS)+'\n');
+            writer.write("Get      |" + ITERATIONS +"           |" + Time.timeGetFromCollection(linkedList, ITERATIONS)+'\n');
+            writer.write("Get      |" + ITERATIONS +"           |" + Time.timeGetFromCollection(arrayList, ITERATIONS)+'\n');
+            writer.write("Delete   |" + ITERATIONS +"           |" + Time.timeDeleteFromCollection(linkedList, ITERATIONS)+'\n');
+            writer.write("Delete   |" + ITERATIONS +"           |"+  Time.timeDeleteFromCollection(arrayList, ITERATIONS)+'\n');
+            writer.flush();
+            writer.close();
+        }
+        catch(IOException ex){
 
+            System.out.println(ex.getMessage());
+        }
 
     }
 }
